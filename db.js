de4e451477b9213b7e0122cbf36391cc9ef4d688
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-09 15:55:53 -0300

    feat: uses database from MongoDB Atlas

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ try {
   const mongoClient = new MongoClient(process.env.MONGO_URL);
   await mongoClient.connect();
   db = mongoClient.db(process.env.DATABASE_NAME);
-  console.log(chalk.green.bold(`\nConnection with database ${chalk.blue.bold(`${db.s.namespace}`)} stablished! \n`));
+  console.log(chalk.green.bold(`\nConnection with database stablished! \n`));
 } catch(e) {
     console.log(chalk.red.bold(`\nWARNING: connection with database failed! \nError: \n`), e);
 }
